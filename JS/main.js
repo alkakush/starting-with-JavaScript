4087170a78b8f7bc7355ea43e2e7c10503ab9d16
Alka kushwaha <crystallife4me@gmail.com>
2021-05-05 16:06:20 +0530

    Used let & const variables

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,32 @@
-console.log('hello world !');
-console.error("This is an error");
-console.warn("This is a warning");
+// let variables
+
+let age = 30;
+
+age = 31;  // we can reassign the value
+
+console.log(age);
+
+
+let score;
+
+score = 10;
+
+console.log(score);
+
+
+//const variable
+
+const mark = 100;
+
+console.log(mark);
+
+
+const number = 90;
+
+number = 95;   // cann't reassign the value
+
+console.log(number);
+
+
+
+
