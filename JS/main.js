f13c8d8be2775da0e8b08bef58ac9b8b7f33cb7f
Alka kushwaha <crystallife4me@gmail.com>
2021-05-06 11:12:29 +0530

    Used JS DataType

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,6 @@
-// String, Number, boolean, null, undefined
+// string , Number, Boolean, null, undefined
 
-
-const name = " John";
+const name = 'John';
 const age = 30;
 const rating = 4.5;
 const isCool = true;
@@ -9,17 +8,18 @@ const x = null;
 const y = undefined;
 let z;
 
-console.log(typeof name);
-console.log(typeof age);
+console.log(typeof name );
+console.log(typeof  age);
 console.log(typeof rating);
 console.log(typeof isCool);
 console.log(typeof x);
 console.log(typeof y);
-console.log(typeof z);
+console.log(typeof z );
 
-//concatenation
 
-const fname = " John";
-const post = 'Manager';
+// concatenation
 
-console.log('my name is ' + fname + ' and I am a ' + post);
+const fname = 'John';
+const post = 'manager';
+
+console.log('My name is ' + fname + ' and I am a ' + post );  
